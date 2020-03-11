5761f0b33e5782947a795be6f6ed2682477c5f8f
Maksim Smolin <maximsmol@gmail.com>
2020-03-11 11:34:10 -0700

    Add HW4 online checkoff

---
+++ hw/conv2d/online_checkoff.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from hw import maxpool2d, conv2d_full
+from utils import imload, imsave
+
+kodim = imload('kodim20.png')
+
+k1 = np.array([
+    [ -1.,  1.,  3.],
+    [  1.,  5., -1.],
+    [ -3., -6.,  1.]
+])
+k2 = np.array([
+    [ -1.,   4.,   3.],
+    [-17.,  17., -10.],
+    [ -3.,  -4.,   1.]
+])
+k3 = np.array([
+    [ 7., 19.,   0.],
+    [-7.,  0., -19.],
+    [ 0.,  3.,  -3.]
+])
+
+stack1 = np.stack([k1, k1, k3], axis=2)
+stack2 = np.stack([k3, k1, k3], axis=2)
+stack3 = np.stack([k3, k2, k2], axis=2)
+
+test = conv2d_full(kodim, [stack1, stack2, stack3], stride=3)
+test = maxpool2d(test, size=7, stride=5)
+
+imsave(test, 'online_checkoff.png')
